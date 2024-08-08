e691b778d61e431a5b3d08d90fc2e50bb1e97104
罗超然 <manonloki@163.com>
2024-08-08 15:25:13 +0800

    features: finish metadata

---
+++ crm-metadata/src/lib.rs
@@ -0,0 +1,43 @@
+use std::pin::Pin;
+mod abi;
+use config::AppConfig;
+use futures::Stream;
+use pb::{
+    metadata_server::{Metadata, MetadataServer},
+    Content, MaterializeRequest,
+};
+use tonic::{async_trait, Request, Response, Status, Streaming};
+
+pub mod config;
+mod pb;
+
+type ServiceResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<Content, Status>> + Send>>;
+
+#[allow(dead_code)]
+pub struct MetadataService {
+    config: AppConfig,
+}
+
+#[async_trait]
+impl Metadata for MetadataService {
+    type MaterializeStream = ResponseStream;
+
+    async fn materialize(
+        &self,
+        request: Request<Streaming<MaterializeRequest>>,
+    ) -> ServiceResult<Self::MaterializeStream> {
+        let query = request.into_inner();
+        self.materialize(query).await
+    }
+}
+
+impl MetadataService {
+    pub fn new(config: AppConfig) -> Self {
+        MetadataService { config }
+    }
+
+    pub fn into_server(self) -> MetadataServer<impl Metadata> {
+        MetadataServer::new(self)
+    }
+}
